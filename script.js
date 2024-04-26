44a64e2f5093d4e1c4cdabf8f2d357d79b85431c
Robert Bruce de Saint George <robertb.dev@gmail.com>
2024-04-25 21:51:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+/*Script do projeto gamebook */
+
+
+/*SCRIPT DA PÁGINA HOME */
+
+/*SCRIPT área artigos */
+
+
+/*FIM da área artigos */
+
+
+
+
+
+
+/*INICIO da área fanmade*/
+
+
+
+/*FIM da área fanmade*/
